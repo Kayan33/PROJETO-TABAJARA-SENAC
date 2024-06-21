e55e815d733ae29c56569d9ccedb699ccf71a47e
Kayan33 <kayanpereira33@gmail.com>
2024-06-21 13:29:46 -0300

    refatoração input

--- src/components/Imc-calorias-1.js
+++ src/components/Imc-calorias-1.js
@@ -1,8 +1,18 @@
 import "../styles/Imc-calorias-1.css";
 import logo from "../assets/homem.png";
-import React from "react";
+import React, { useState } from "react";
 
 function Calorias01() {
+const[idade, setIdade] = useState('')
+const[altura, setAltura] = useState('')
+const[peso, setPeso] = useState('')
+const[sexo, setSexo] = useState('')
+const[objetivo, setObjetivo] = useState('')
+const[exercicio, setExercicio] = useState('')
+const[tipoEmprego, setTipoEmprego] = useState('')
+const[masculino, setTiMasculino] = useState('')
+const[feminino, setTiFeminino] = useState('')
+ 
   return (
     <main className="container">
       <article className="calorie-calculator">
@@ -13,32 +23,50 @@ function Calorias01() {
             <form className="form">
               <div>
                 <p>Idade:</p>
-                <input type="number" />
+                <input type="number" 
+                value={idade}
+                onChange={(e) => setIdade(e.target.value)}/>
               </div>
               <div>
                 <p>Altura:</p>
-                <input type="number" />
+                <input type="number" 
+                value={altura}
+                onChange={(e) => setAltura(e.target.value)}/>
               </div>
               <div>
                 <p>Peso atual:</p>
-                <input type="number" />
+                <input type="number" 
+                value={peso}
+                onChange={(e) => setPeso(e.target.value)}/>
               </div>
 
-              <div>
-                <p>Sexo:</p>
-                <input type="text" />
-              </div>
+
               <div>
                 <p>Objetivo:</p>
-                <input type="text" />
+                <input type="text" 
+                value={objetivo}
+                onChange={(e) => setObjetivo(e.target.value)}/>
               </div>
               <div>
                 <p>Exercício semanal:</p>
-                <input type="text" />
+                <input type="text" 
+                value={exercicio}
+                onChange={(e) => setExercicio(e.target.value)}/>
               </div>
               <div>
                 <p>Tipo de emprego:</p>
-                <input type="text" />
+                <input type="text" 
+                value={tipoEmprego}
+                onChange={(e) => setTipoEmprego(e.target.value)}/>
+              </div>
+              <div>
+                <p>Sexo:</p>
+                <select 
+                value={sexo}
+                onChange={(e) => setSexo(e.target.value)}>
+                  <option value={masculino}>Masculono</option>
+                  <option value={feminino}>Feminino</option>
+                </select>
               </div>
             </form>
           </div>
