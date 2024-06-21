866333256e35c751a87dcdcc52614d0e7d54d570
Danrley Dimas <danrleydimas@outlook.com>
2024-06-21 17:23:51 -0300

    update 1.7

---
+++ src/components/Home-article-2.js
@@ -0,0 +1,27 @@
+import React from "react"
+import mulher from '../assets/mulher.png'
+import { Link } from "react-router-dom"
+import '../styles/home-article-2.css'
+
+export default function Home_Article2() {
+    return (
+        <div className="principal2">
+            <article className="artigo2">
+                <div>
+                    <img src={mulher} alt="Mulher-malhando" className="img2"/>
+                </div>
+                <div className="text2">
+                    <p>Explore nossa variedade de dietas personalizadas
+                    e exercícios especializados para atingir
+                    seus objetivos de saúde. Utilize nossa
+                    Calculadora de Calorias para planejar
+                    suas refeições e descubra rotinas de exercícios
+                    adaptadas às suas necessidades.
+                    Comece sua jornada rumo ao bem-estar hoje mesmo!
+                    <Link to='/calorias'>Calculadora de calorias</Link>
+                    </p>
+                </div>
+            </article>
+        </div>
+    )
+}
